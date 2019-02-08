01e99074534ba74ed754855637f95cc4d8728f5c
Steve Bull <rgitonga07@gmail.com>
2019-02-08 15:39:19 +0300

    [Finishes #163719430]end point allows user to view all parties

--- app/api/v1/views/parties/parties.py
+++ app/api/v1/views/parties/parties.py
@@ -2,6 +2,13 @@ from flask import jsonify, make_response,request, Blueprint
 from app.api.v1.views import bash
 from app.api.v1.models.models import PartiesModel
 
+@bash.route("/parties", methods=["GET"])
+def getparties():
+    return make_response(jsonify({
+        "status": 200,
+        "data": PartiesModel.view_all_parties()
+    }), 200)
+
 @bash.route("/parties", methods=["POST"])
 def postparty():
     data = request.get_json()
